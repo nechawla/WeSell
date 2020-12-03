340308527546aeb5fdcdba9222961f9a02829baf
JordanWaldron <61469822+JordanWaldron@users.noreply.github.com>
2020-12-02 23:59:22 -0500

    Update login.php

--- login.php
+++ login.php
@@ -2,9 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Register Account</title>
-
-
+  <title>Login</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -23,39 +21,51 @@
  <div style="padding-top:20px;padding-left:40px;width:500px;height:600px;">
   <div class="form-group">
        <!--    body  (form)-->            
-        <form role="form" action="./index.php" method="post" id="register">
-        <div class="form-group">
-            <label>First Name</label>
-            <input type="email" name="email" id="inputFirstName" class="form-control" placeholder="First Name " required>
-          </div>
-          <div class="form-group">
-            <label>Last Name</label>
-            <input type="email" name="email" id="inputLastName" class="form-control" placeholder="Last Name " required>
-          </div>
+        <form action="login.php" method="post">
+        
           <div class="form-group">
             <label>Email</label>
             <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email Address " required>
           </div>
           <div class="form-group">
             <label class="control-label">Password</label>
-            <input type="password" name="password" id="pwd" class="form-control" placeholder="Password " required>
-            <div class="help-block">At least 8 characters with at least one Capital letter, at least one lower case letter and at least one number and at least one Special character:</div>
+            <input type="password" name="password" id="pwd" class="form-control" placeholder="Password " required>                         
           </div>
+          <!-- Submib button -->
           <div class="form-group">
-            <input type="password" name="repassword" id="repwd" class="form-control" placeholder="Re-type Password " required>
+            <button type="submit"  >Login</button>
           </div>
-        
-        </form>                  
-    </div>
-    <!-- Submib button -->
-    <div class="form-group">
-      <button class="btn btn-success btn-block" name="submit" onclick = "return registerValidation()"  form="register">Register</button>
-    </div>
-  </form>
+        </form>
   
 </div>
 
-<?php include('include/footer.php') ;?>
+<?php 
+  if(isset($_POST["submit"])){
+    if(!empty($_POST['email']) && !empty($_POST['password'])) {
+      $loginEmail=$_POST['email'];
+      $loginPass=$_POST['password'];
 
+      $servername = "localhost";$username = "root";$password = "";$database="weselldb";
+      // Create connection
+      $con = new mysqli($servername, $username, $password,$database);
+      $query="SELECT * FROM customer WHERE email='".$loginEmail."' AND password='".$loginPass."'";  
+      $result= mysqli_query($con, $query);
+      $num = mysqli_num_rows($result);
+      //need to add the encryption for password check, tested with a hardcoded user
+   
+      if($num == 1){
+        $_SESSION['email'] = $loginEmail;
+        header("location: homePage.php");
+        //exit();
+      }else  
+      {
+        header("location: login.php");
+        //exit();
+      }
+    }  
+  } 
+
+?>
 </body>
+
 </html>
